ef578fde698ccec25d1fceac0711f704f7b505b1
Udit Desai <desaiuditd@gmail.com>
2017-03-02 09:56:34 -0800

    adds session counter for website visit

--- imports/ui/components/appNav/appNav.js
+++ imports/ui/components/appNav/appNav.js
@@ -2,4 +2,23 @@
  * Created by udit on 2/28/17.
  */
 
+import { Session } from 'meteor/session'
 import "./appNav.html";
+
+Template.appNav.onRendered(() => {
+  let visitCount = Session.get('visitCount');
+
+  if(typeof visitCount === 'undefined') {
+    Session.setPersistent('visitCount', 0);
+  }
+
+  visitCount = Session.get('visitCount');
+  Session.update('visitCount', ++visitCount);
+});
+
+Template.appNav.helpers({
+  visitCount: function () {
+    const visitCount = Session.get('visitCount');
+    return typeof visitCount === 'undefined' ? 0 : visitCount;
+  },
+});
